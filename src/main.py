67e40bb1dd5e089d13dba4b83ff2e244c803fb7d
Beskhue <thomas@kepow.org>
2017-05-18 00:52:18 +0200

    Add transformers to main.

--- src/main.py
+++ src/main.py
@@ -11,6 +11,17 @@ def run():
     logger.info("Starting...")
 
     loader = data.Loader()
+    train_data = loader.load_original_images()
+    train_val_split = loader.train_val_split(train_data)
+
+    transform = data.LoadTransformer(data.AugmentationTransformer())
+    iterator = data.DataIterator(train_val_split['train'], transform, batch_size = 8, shuffle = True, seed = 42);
+
+    batch = next(iterator)
+
+    print("First in batch: %s" % batch[0]['m'])
+    print("Batch size: %s" % len(batch))
+    
 
 if __name__ == "__main__":
     run()
