7cb0cd231b235454893a4a35fa123efbfb8199d7
Jordi <jordi.riemens@student.ru.nl>
2017-06-10 21:00:38 +0200

    Rewrite HoGE feature to vigra framework

--- src/feature.py
+++ src/feature.py
@@ -3,8 +3,6 @@ import numpy as np
 import os
 import scipy
 import settings
-from skimage import img_as_float
-from skimage.feature import hessian_matrix, hessian_matrix_eigvals
 import vigra
 
 DEFAULT_SCALES = [0.3, 0.7, 1.0, 1.6, 3.5, 5.0, 10.0] # in pixels
@@ -31,21 +29,14 @@ def gaussian_smoothing_and_difference_of_gaussians(img, scale):
     return gs, dog
 
 def hessian_of_gaussian_eigenvalues(img, scale):
-    outimg = np.empty_like(img)
-    outimg2 = np.empty_like(img)
-    for d in range(3):
-        Hxx, Hxy, Hyy = hessian_matrix(img[:,:,d], sigma=scale, order='rc')
-        outimg[:,:,d], outimg2[:,:,d] = hessian_matrix_eigvals(Hxx, Hxy, Hyy)
-    return outimg, outimg2
+    return vigra.filters.hessianOfGaussianEigenvalues(np.sum(img, axis=2), scale=scale)
     
 def structure_tensor_eigenvalues(img, scale):
-    outimg = vigra.filters.structureTensorEigenvalues(img, innerScale=scale, outerScale=0.5*scale)
-    return outimg
+    return vigra.filters.structureTensorEigenvalues(img, innerScale=scale, outerScale=0.5*scale)
 
 def generate_features(img, base_out_path, scales=DEFAULT_SCALES):
     # base_out_path is "path/to/output/dir/imageid"
     img = preproc(img)
-    imgt = np.transpose(img, (1,0,2)) # HoGE is from scipy which does not respect axistags
     
     for scale in scales:
         settings.logger.info('Generating scale = ' + ('%g' % scale) + ' features...')
@@ -60,9 +51,9 @@ def generate_features(img, base_out_path, scales=DEFAULT_SCALES):
         out.writeImage(outpath + 'gs.jpg')
         out2.writeImage(outpath + 'dog.jpg')
         
-        out, out2 = hessian_of_gaussian_eigenvalues(imgt, scale)
-        scipy.misc.imsave(outpath + 'hoge1.jpg', out)
-        scipy.misc.imsave(outpath + 'hoge2.jpg', out2)
+        out = hessian_of_gaussian_eigenvalues(img, scale)
+        (out[:,:,0]).writeImage(outpath + 'hoge1.jpg')
+        (out[:,:,1]).writeImage(outpath + 'hoge2.jpg')
         
         out = structure_tensor_eigenvalues(img, scale)
         (out[:,:,0]).writeImage(outpath + 'ste1.jpg')
@@ -86,11 +77,10 @@ def run_feature_generation(dataset, start=0, end=-1):
         end = len(images)
     
     for idx in range(start, end):
-        settings.logger.info('Generating features for image ' + str(idx) + '...')
-        
         imageid = images[idx]['m']['filename']
-        image = vigra.readImage(os.path.join(settings.TRAIN_ORIGINAL_IMAGES_DIR, imageid + '.jpg'))
+        settings.logger.info('Generating features for image %d (%s.jpg)...' % (idx, imageid))
         
+        image = vigra.readImage(os.path.join(settings.TRAIN_ORIGINAL_IMAGES_DIR, imageid + '.jpg'))
         generate_features(image, os.path.join(outdir, imageid))
 
 
