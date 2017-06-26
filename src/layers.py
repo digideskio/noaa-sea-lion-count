f222c9ed12004926ed4653c1a2a8cebac8af7d67
Beskhue <thomas@kepow.org>
2017-06-26 21:15:48 +0200

    Simplify.

--- src/layers.py
+++ src/layers.py
@@ -3,13 +3,6 @@ from keras.engine.topology import Layer
 
 class DensityCount(Layer):
 
-    #def __init__(self, output_dim, **kwargs):
-    #    self.output_dim = output_dim
-    #    super(DensityCount, self).__init__(**kwargs)
-
-    #def build(self, input_shape):
-    #    super(DensityCount, self).build(input_shape)  # Be sure to call this somewhere!
-
     def call(self, x):
         return K.sum(x, axis=(1,2,3))
 
